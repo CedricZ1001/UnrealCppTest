ccdeaf396afce9125e9dc79818b7c8cfb1980cf5
CedricZ1001 <Cedric1001Z@gmail.com>
2024-05-17 14:05:24 +0800

    init project

---
+++ Source/TestDemo/TestDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTDEMO_API ATestDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
